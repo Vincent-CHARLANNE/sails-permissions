70086d3aece390d85d6c2533cbe78b8da976e19f
Armel Larcier <armel.larcier@jpm-next.com>
2016-11-14 14:29:29 +0100

    PermissionService.addUsersToRole will check whether the role already has the user associated to it

--- api/services/PermissionService.js
+++ api/services/PermissionService.js
@@ -320,6 +320,9 @@ module.exports = {
       return User.find({
         username: usernames
       }).then(function(users) {
+        users = _.filter(users, function(user){
+          return !_.includes(usernames, user.username);
+        })
         role.users.add(_.pluck(users, 'id'));
         return role.save();
       });
